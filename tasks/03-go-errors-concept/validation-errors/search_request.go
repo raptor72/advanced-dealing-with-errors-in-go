f1aff1d4da5f47e3145fa929589ebc0ca58434b6
VTB217707 <tarasov.ms@dev.vtb>
2025-02-17 22:56:12 +0400

    validate()

--- tasks/03-go-errors-concept/validation-errors/search_request.go
+++ tasks/03-go-errors-concept/validation-errors/search_request.go
@@ -2,8 +2,9 @@ package requests
 
 import (
 	// Доступные пакеты, _ для сохранения импортов.
-	_ "errors"
-	_ "fmt"
+	"errors"
+	"fmt"
+	// _ "fmt"
 	"regexp"
 	_ "strings"
 )
@@ -14,24 +15,86 @@ const maxPageSize = 100
 var (
 	// errIsNotRegexp     error
 	errIsNotRegexp     ErrIsNotRegexp
-	errInvalidPage     error
-	errInvalidPageSize error
+	errInvalidPage     ErrInvalidPage
+	errInvalidPageSize ErrInvalidPageSize
 )
 
 type ErrIsNotRegexp struct {
 	Msg    string
 	Origin error
 }
+
 func (e ErrIsNotRegexp) Error() string {
 	return e.Msg + e.Origin.Error()
+    // return e.Msg
+	// return fmt.Errorf("%v", e.Origin)
 }
 func (e ErrIsNotRegexp) Unwrap() error {
-    return e.Origin
+	return e.Origin
 }
 
+type ErrInvalidPage struct {
+	Msg    string
+	Page   int
+	Origin error
+}
 
-// Реализуй мои методы.
+func (e ErrInvalidPage) Error() string {
+	return e.Msg + fmt.Sprintf("%d", e.Page)
+}
+func (e ErrInvalidPage) Unwrap() error {
+	return e.Origin
+}
+
+type ErrInvalidPageSize struct {
+	Msg      string
+    Comp     string
+    Val      string
+	PageSize int
+	Origin   error
+}
+
+func (e ErrInvalidPageSize) Error() string {
+	return e.Msg + fmt.Sprintf("%v %v %v", e.PageSize, e.Comp, e.Val)
+	// return e.Msg
+}
+func (e ErrInvalidPageSize) Unwrap() error {
+	return e.Origin
+}
+
+// type ValidationErrors struct {
+// 	vErrs []error
+// }
+
+// func (verr ValidationErrors) Error() string {
+// 	var resErr string
+// 	for _, e := range verr{
+// 		s += e.Error()
+// 	}
+// 	return s
+// }
+
 type ValidationErrors []error
+func (verr ValidationErrors) Error() string {
+    var s string
+	if len(verr) != 0 {
+    	s += "validation errors:\n"
+	    for _, e := range verr{
+		    s += "\t" + e.Error() + "\n"
+    	}
+    }
+	return s
+}
+
+func (verr ValidationErrors) Is(target error) bool {
+	for _, e := range verr {
+		if errors.Is(e, target) {
+			return true
+		}
+	}
+	return false
+}
+
 
 type SearchRequest struct {
 	Exp      string
@@ -41,10 +104,25 @@ type SearchRequest struct {
 
 func (r SearchRequest) Validate() error {
 	// Реализуй меня.
-	_, err := regexp.Compile(r.Exp)
-    if err != nil {
-		return ErrIsNotRegexp{Msg: "exp is not regexp: ", Origin: err}
-	}
+	resErrors := ValidationErrors{}
 
-	return nil
+	_, err := regexp.Compile(r.Exp)
+	if err != nil {
+		// return ErrIsNotRegexp{Msg: "exp is not regexp: ", Origin: err}
+		regErr := ErrIsNotRegexp{Msg: "exp is not regexp: ", Origin: err}
+		resErrors = append(resErrors, regErr)
+	}
+	if r.Page <= 0 {
+		pageErr := ErrInvalidPage{Msg: "invalid page:", Page: r.Page}
+		resErrors = append(resErrors, pageErr)
+	}
+	if r.PageSize <= 0 {
+		pageSizeErr := ErrInvalidPageSize{Msg: "invalid page size:", Comp: " < ", Val: "0", PageSize: r.PageSize}
+		resErrors = append(resErrors, pageSizeErr)
+	}
+	if r.PageSize > maxPageSize {
+		pageSizeErr := ErrInvalidPageSize{Msg: "invalid page size:", Comp: ">", Val: fmt.Sprintf("%d", maxPageSize), PageSize: r.PageSize}
+		resErrors = append(resErrors, pageSizeErr)
+	}
+	return resErrors
 }
