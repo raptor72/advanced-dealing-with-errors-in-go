b82c17914ddea27a956900e12e10d6205979e04a
VTB217707 <tarasov.ms@dev.vtb>
2025-02-26 22:37:35 +0400

    transfeer

--- tasks/03-go-errors-concept/validation-errors/search_request.go
+++ tasks/03-go-errors-concept/validation-errors/search_request.go
@@ -7,73 +7,24 @@ import (
 
 	// _ "fmt"
 	"regexp"
-	"strings"
+	_ "strings"
 )
 
 const maxPageSize = 100
 
-// Реализуй нас.
 var (
-	// errIsNotRegexp     error
-	errIsNotRegexp     ErrIsNotRegexp
-	errInvalidPage     ErrInvalidPage
-	errInvalidPageSize ErrInvalidPageSize
+	errIsNotRegexp     error
+	errInvalidPage     error
+	errInvalidPageSize error
+	validationErrors   error
 )
 
-type ErrIsNotRegexp struct {
-	Msg    string
-	Origin error
-}
-
-func (e ErrIsNotRegexp) Error() string {
-	if e.Origin != nil {
-//        return e.Msg + e.Origin.Error()
-		return fmt.Errorf("%v%w", e.Msg, e.Origin).Error()
-	}
-	return e.Msg
-}
-
-func (e ErrIsNotRegexp) Unwrap() error {
-	if e.Origin != nil {
-        return e.Origin
-	}
-	return nil
-}
-
-func (e *ErrIsNotRegexp) Is(target error) bool {
-    return strings.Contains(target.Error(), e.Msg)
-}
-
-type ErrInvalidPage struct {
-	Msg    string
-	Page   int
-	Origin error
-}
-
-func (e ErrInvalidPage) Error() string {
-	return e.Msg + fmt.Sprintf("%d", e.Page)
-}
-func (e ErrInvalidPage) Unwrap() error {
-	return e.Origin
-}
-
-type ErrInvalidPageSize struct {
-	Msg      string
-    Comp     string
-    Val      string
-	PageSize int
-	Origin   error
-}
-
-func (e ErrInvalidPageSize) Error() string {
-	return e.Msg + fmt.Sprintf("%v %v %v", e.PageSize, e.Comp, e.Val)
-	// return e.Msg
-}
-func (e ErrInvalidPageSize) Unwrap() error {
-	return e.Origin
-}
 
 type ValidationErrors []error
+// type ValidationErrors struct {
+	// errs []error
+// }
+
 func (verr ValidationErrors) Error() string {
     var s string
 	if len(verr) != 0 {
@@ -85,22 +36,17 @@ func (verr ValidationErrors) Error() string {
 	return s
 }
 
-func (verr ValidationErrors) Unwrap() error {
-	if len(verr) != 0 {
-        for _, e := range verr {
-			return e
-		}
-	}
-	return nil
-}
+// func (verr ValidationErrors) Unwrap() error {
+// 	if len(verr) != 0 {
+//         for _, e := range verr {
+// 			return e
+// 		}
+// 	}
+// 	return nil
+// }
 
 func (verr ValidationErrors) Is(target error) bool {
-	for _, e := range verr {
-		if errors.Is(e, target) {
-			return true
-		}
-	}
-	return false
+	return errors.Is(verr, target)
 }
 
 type SearchRequest struct {
@@ -109,29 +55,40 @@ type SearchRequest struct {
 	PageSize int
 }
 
-func (r SearchRequest) Validate() error {
-	// Реализуй меня.
-	resErrors := ValidationErrors{}
+func (r SearchRequest) Validate() ValidationErrors {
+    var vers ValidationErrors
+	haserrs := false
+	var validationErrors = errors.New("validation errors:")
 	_, err := regexp.Compile(r.Exp)
 	if err != nil {
-		regErr := ErrIsNotRegexp{Msg: "exp is not regexp: ", Origin: err}
-		// regErr := ErrIsNotRegexp{Msg: "error parsing regexp: ", Origin: err}
-		resErrors = append(resErrors, regErr)
+		// var errIsNotRegexp = errors.New("\texp is not regexp: ")
+		var errIsNotRegexp = errors.New("exp is not regexp: ")
+		errIsNotRegexp = fmt.Errorf("%v%w", errIsNotRegexp, err)
+		validationErrors = fmt.Errorf("%v\n%w", validationErrors, errIsNotRegexp)
+        vers = append(vers, errIsNotRegexp)
+		haserrs = true
 	}
 	if r.Page <= 0 {
-		pageErr := ErrInvalidPage{Msg: "invalid page: ", Page: r.Page}
-		resErrors = append(resErrors, pageErr)
-	}
-	if r.PageSize <= 0 {
-		pageSizeErr := ErrInvalidPageSize{Msg: "invalid page size: ", Comp: "<", Val: "0", PageSize: r.PageSize}
-		resErrors = append(resErrors, pageSizeErr)
+		var errInvalidPage = fmt.Errorf("invalid page: %v", r.Page)
+		validationErrors = fmt.Errorf("%v\n%w", validationErrors, errInvalidPage)
+		vers = append(vers, errInvalidPage)
+		haserrs = true
 	}
 	if r.PageSize > maxPageSize {
-		pageSizeErr := ErrInvalidPageSize{Msg: "invalid page size: ", Comp: ">", Val: fmt.Sprintf("%d", maxPageSize), PageSize: r.PageSize}
-		resErrors = append(resErrors, pageSizeErr)
+		var errInvalidPageSize = fmt.Errorf("invalid page size: %v > %v", r.PageSize, maxPageSize)
+		validationErrors = fmt.Errorf("%v\n%w", validationErrors, errInvalidPageSize)
+		vers = append(vers, errInvalidPageSize)
+		haserrs = true
 	}
-	if len(resErrors) != 0 {
-		return resErrors
+	if r.PageSize <= 0 {
+		var errInvalidPageSize = fmt.Errorf("invalid page size: %v < 0", r.PageSize)
+		validationErrors = fmt.Errorf("%v\n%w", validationErrors, errInvalidPageSize)
+		vers = append(vers, errInvalidPageSize)
+		haserrs = true
+	}
+	if haserrs {
+	    return vers
+		// return validationErrors
 	}
 	return nil
 }
